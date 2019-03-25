1a5b01fb720d3401e2f7cd1a8ebb8dcb018f6e97
Prashant-Mutnale <prashantmutnale@gmail.com>
2019-03-25 23:28:49 +0530

    changed the https

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-import http from 'http';
+import http from "http";
+import https from "https";
 
-let app = require('./server').default;
-const server = http.createServer(app);
+let app = require("./server").default;
+const server = https.createServer(app);
 
 server.listen(process.env.PORT, error => {
   if (error) {
     console.log(error);
   }
-  console.log(`React SSR App is running: http://localhost:${process.env.PORT}`);
+  console.log(
+    `React SSR App is running: https://localhost:${process.env.PORT}`
+  );
 });
 
 let currentApp = app;
 if (module.hot) {
-  module.hot.accept('./server', () => {
-    console.log('Server reloading...');
+  module.hot.accept("./server", () => {
+    console.log("Server reloading...");
 
     try {
-      app = require('./server').default;
-      server.removeListener('request', currentApp);
-      server.on('request', app);
+      app = require("./server").default;
+      server.removeListener("request", currentApp);
+      server.on("request", app);
       currentApp = app;
     } catch (error) {
       // Do nothing
